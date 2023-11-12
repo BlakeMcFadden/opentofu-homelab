2b3bb8c25645c35f501f8d30c1f79866e4bf3760
Dairfey <blakemcfadden05@gmail.com>
2023-11-12 16:31:09 -0500

    Adjusting Proxmox Provider

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     proxmox = {
       source = "telmate/proxmox"
-      version = "2.7.4"
+      version = "2.9.14"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url = vars.api_url
+  pm_api_url = var.api_url
   pm_api_token_id = var.token_id
   pm_api_token_secret = var.token_secret
   pm_tls_insecure = true # Change to false if you have your 
